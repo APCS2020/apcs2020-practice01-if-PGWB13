6fa043ad2f7562d7a49a330ae589523fcf26204c
戴帛洋 David <50470598+PGWB13@users.noreply.github.com>
2019-05-17 11:11:19 +0800

    Create weekday.java

---
+++ weekday.java
@@ -0,0 +1,9 @@
+public class weekday{
+    public static void main(String[] args){
+        int n = 3;
+        if int = 1;
+        System.out.println("Monday!");
+        if int = 2;
+        System.out.println("Tuesday!")
+    }
+}
